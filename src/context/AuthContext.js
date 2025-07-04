084415d8b233114105f769c33e2d144414a8f6fe
JaganSivanantham <117537577+JaganSivanantham@users.noreply.github.com>
2025-07-04 22:33:38 +0530

    Update AuthContext.js

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -10,7 +10,7 @@ export const AuthProvider = ({ children }) => {
 
     // In src/context/AuthContext.js
 // and in src/api/wishlistApi.js
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://wishlist-api-l75s.onrender.com/api';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
 
     useEffect(() => {
         const validateToken = async () => {
