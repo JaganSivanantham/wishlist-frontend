cad72e398904a6c0bc604fbd36a49430385ffc79
JaganSivanantham <117537577+JaganSivanantham@users.noreply.github.com>
2025-07-04 22:22:15 +0530

    Update wishlistApi.js

--- src/api/wishlistApi.js
+++ src/api/wishlistApi.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:65091/api'; // Or from AuthContext if you prefer
+// In src/context/AuthContext.js
+// and in src/api/wishlistApi.js
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://wishlist-api-l75s.onrender.com/api'; // Or from AuthContext if you prefer
 
 const getAuthHeaders = () => {
     const token = localStorage.getItem('token');
