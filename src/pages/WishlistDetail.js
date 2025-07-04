01d92706d5e01b4f249248720e7725d391816b66
JaganSivanantham <117537577+JaganSivanantham@users.noreply.github.com>
2025-07-04 22:45:41 +0530

    Update WishlistDetail.js

--- src/pages/WishlistDetail.js
+++ src/pages/WishlistDetail.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import { useCallback } from 'react';
 import wishlistApi from '../api/wishlistApi';
 
 const WishlistDetail = () => {
