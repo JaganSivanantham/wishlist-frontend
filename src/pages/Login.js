a10f7539d48a26c31f8731d73a24dd5db3f93e53
JaganSivanantham <117537577+JaganSivanantham@users.noreply.github.com>
2025-07-04 22:57:26 +0530

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+
 import { useAuth } from '../context/AuthContext';
 import wishlistApi from '../api/wishlistApi';
 
